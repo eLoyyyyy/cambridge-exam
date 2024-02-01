dcbcad8a21c20502d9d3a0d2c9190c02f6624ee0
Zyb Jared Valdez <zybjared@gmail.com>
2024-02-01 17:14:10 +0800

    question page

--- pages/ask.tsx
+++ pages/ask.tsx
@@ -1,3 +1,29 @@
+import { useGetQuestions } from "../hooks/hooks"
+import { Heading, StackDivider, VStack } from "@chakra-ui/react"
+import Question from "../components/Question";
+
 export default function Page () {
-    
+    const {
+        isLoading,
+        isError,
+        data,
+    } = useGetQuestions();
+
+    if (isLoading) {
+        return <Heading>Loading ...</Heading>
+    }
+
+    if (isError) {
+        <Heading>Error has occured.</Heading>
+    }
+
+    return (
+        <VStack
+            divider={<StackDivider borderColor='gray.200' />}
+        >
+            {data.questions.map((questionProps) => (
+                <Question key={questionProps.id} {...questionProps} />
+            ))}
+        </VStack>
+    )
 }
